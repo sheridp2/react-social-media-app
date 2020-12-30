297b23baa74029cea973bdd20c0fe404d574a063
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-30 10:55:51 -0800

    getting test posts from database

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const Post = require('./models/Post')
 const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+
     type Query{
-        sayHi: String
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World' 
+        async getPosts(){
+            try{
+                const posts = await Post.find()
+                return posts
+            } catch(err){
+                throw new Error(err)
+            }
+        }
     }
 }
 
