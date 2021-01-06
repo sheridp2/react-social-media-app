73cecb69951218d300dc71a70a6edf96e94a3119
Patrick Sheriddan <sheridp2@gmail.com>
2021-01-06 11:42:10 -0800

    user login and error handling working, returns user data and token

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,16 +2,68 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInoutError, UserInputError } = require("apollo-server");
 
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
       //TODO Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
       //TODO make sure user doesn't already exist
       const user = await User.findOne({ username });
 
@@ -33,15 +85,8 @@ module.exports = {
       });
 
       const res = await newUser.save();
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+
+      const token = generateToken(res);
       return {
         ...res._doc,
         id: res._id,
