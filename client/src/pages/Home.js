d0033381875adb7ee4749a167d00a99e07e1cbfb
Patrick Sheriddan <sheridp2@gmail.com>
2021-01-14 14:08:26 -0800

    all post displaying on recents post page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,58 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { Grid, Transition } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 function Home() {
+  const { loading, data: { getPosts: posts } = {} } = useQuery(
+    FETCH_POSTS_QUERY
+  );
+
   return (
-    <div>
-      <div>
-        <h1>Home Page</h1>
-      </div>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
